1aa96deb49fd0f3006706bc25572457eec0dfb0c
GabrielTeixeiraC <69649169+GabrielTeixeiraC@users.noreply.github.com>
2022-09-29 22:56:22 -0300

    feat: criando arquivos .ts

---
+++ api/hello.ts
@@ -0,0 +1,6 @@
+// This is an industrial-grade general-purpose greeter function:
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date}!`);
+}
+   
+greet("Brendan");
