5ff79a7e86b073335a682acd2a1400d7bda16a2e
gmiserani <gabrielamiserani@uol.com.br>
2022-10-07 13:20:10 -0300

    feat: atualizar index users

--- api/src/domains/users/controllers/index.ts
+++ api/src/domains/users/controllers/index.ts
@@ -1,17 +1,19 @@
-const router = require('express').Router();
-const UserService = require('../services/UserService');
-const {loginMiddleware,
+import {Router, Request, Response, NextFunction} from 'express';
+import {UserService} from '../services/UserService';
+import {loginMiddleware,
   verifyJWT,
   checkRole,
-  notLoggedIn} = require('../../../middlewares/auth-middlewares.js');
-const userRoles = require('../../users/constants/userRoles.js');
-const statusCodes = require('../../../../constants/statusCodes.js');
+  notLoggedIn} from '../../../middlewares/auth-middlewares.js';
+import {userRoles} from '../../users/constants/userRoles.js';
+import {statusCodes} from '../../../../constants/statusCodes.js';
+
+export const router = Router();
 
 router.post('/login', notLoggedIn, loginMiddleware);
 
 router.post('/logout',
-  verifyJWT,
-  async (req, res, next) => {
+verifyJWT,
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       res.clearCookie('jwt');
       res.status(statusCodes.noContent).end();
@@ -22,7 +24,7 @@ router.post('/logout',
 );
 
 router.post('/',
-  async (req, res, next) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       await UserService.create(req.body);
       res.status(statusCodes.created).end();
@@ -34,7 +36,7 @@ router.post('/',
 
 router.get('/',
   verifyJWT,
-  async (req, res, next) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       const users = await UserService.getAll();
       res.status(statusCodes.success).json(users);
@@ -47,9 +49,9 @@ router.get('/',
 
 router.get('/user',
   verifyJWT,
-  async (req, res, next) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-      if (req.user) {
+      if (req.user) { 
         const user = await UserService.getById(req.user.id);
         res.status(statusCodes.success).json(user);
       }
@@ -57,12 +59,12 @@ router.get('/user',
       next(error);
     }
   },
-);
+  );
 
 
 router.get('/:id',
   verifyJWT,
-  async (req, res, next) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       const user = await UserService.getById(req.params.id);
 
@@ -76,7 +78,7 @@ router.get('/:id',
 
 router.put('/:id',
   verifyJWT,
-  async (req, res, next) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       await UserService.update(req.params.id, req.body, req.user);
       res.status(statusCodes.noContent).end();
@@ -89,7 +91,7 @@ router.put('/:id',
 router.delete('/:id',
   verifyJWT,
   checkRole([userRoles.admin]),
-  async (req, res, next) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       await UserService.delete(req.params.id, req.user.id);
       res.status(statusCodes.noContent).end();
@@ -98,4 +100,3 @@ router.delete('/:id',
     }
   });
 
-module.exports = router;
