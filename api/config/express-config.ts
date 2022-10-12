af9afa080709669cf45c3800a695a57e94a1c94a
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-10-12 18:17:48 -0300

    feat: adicionando routers ao express config
    Co-authored-by: gmiserani <gmiserani@users.noreply.github.com>
    Co-authored-by: iriasmatheus <iriasmatheus@users.noreply.github.com>

--- api/config/express-config.ts
+++ api/config/express-config.ts
@@ -24,14 +24,14 @@ app.use(express.json());
 import { router as usersRouter } from '../src/domains/users/controllers/index';
 app.use('/api/users', usersRouter);
 
-// import { artistsRouter } from '../src/domains/artists/controllers/index.js';
-// app.use('/api/artists', artistsRouter);
+import { router as artistsRouter } from '../src/domains/artists/controllers/index';
+app.use('/api/artists', artistsRouter);
 
-// import { songsRouter } from '../src/domains/songs/controllers/index.js';
-// app.use('/api/songs', songsRouter);
+import { router as songsRouter } from '../src/domains/songs/controllers/index';
+app.use('/api/songs', songsRouter);
 
-// import { usersSongsRouter } from '../src/domains/userSongs/controllers/index.js';
-// app.use('/api/users-songs', usersSongsRouter);
+import { router as usersSongsRouter } from '../src/domains/userSongs/controllers/index';
+app.use('/api/users-songs', usersSongsRouter);
 
-import { errorHandler } from '../src/middlewares/error-handler.js';
+import { errorHandler } from '../src/middlewares/error-handler';
 app.use(errorHandler);
