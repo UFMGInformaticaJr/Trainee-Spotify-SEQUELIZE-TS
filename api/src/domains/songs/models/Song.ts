df46de6cdc6c02f792287ab45f561c4579c37d42
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-10-12 18:19:40 -0300

    feat: atualiza model song
    Co-authored-by: gmiserani <gmiserani@users.noreply.github.com>
    Co-authored-by: iriasmatheus <iriasmatheus@users.noreply.github.com>

---
+++ api/src/domains/songs/models/Song.ts
@@ -0,0 +1,59 @@
+import { sequelize } from '../../../../database';
+import { DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional, HasManyAddAssociationMixin, HasManyRemoveAssociationMixin} from 'sequelize';
+import { Artist } from '../../artists/models/Artist';
+import { UserInterface } from '../../users/models/User';
+import { defaultImage } from '../constants/defaultImage';
+
+export interface SongInterface extends Model<InferAttributes<SongInterface>, InferCreationAttributes<SongInterface>> {
+  id: CreationOptional<string>;
+  title: string;
+  cover_image: string;
+  artist_id: string;
+  genre: string;
+  addUser: HasManyAddAssociationMixin<UserInterface, UserInterface['id']>;
+  removeUser: HasManyRemoveAssociationMixin<UserInterface, UserInterface['id']>;
+}
+
+export const Song = sequelize.define<SongInterface>('Songs', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cover_image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: defaultImage,
+  },
+  artist_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  genre: { 
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
+
+Song.belongsTo(Artist, {
+  foreignKey: 'artist_id',
+});
+
+Artist.hasMany(Song, {
+  foreignKey: 'artist_id',
+});
+
+/*
+Comando para criar/alterar as
+colunas da tabela caso necessário
+ */
+Song.sync({alter: true, force: false})
+  .then(() => {
+    console.log('Song table was (re)created');
+  })
+  .catch((err) => console.log(err));
