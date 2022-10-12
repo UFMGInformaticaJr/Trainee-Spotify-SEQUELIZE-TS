bd1ce7806e113f869283f895555e966700b04d36
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-10-12 18:19:02 -0300

    feat: atualiza model artists
    Co-authored-by: gmiserani <gmiserani@users.noreply.github.com>
    Co-authored-by: iriasmatheus <iriasmatheus@users.noreply.github.com>

---
+++ api/src/domains/artists/models/Artist.ts
@@ -0,0 +1,39 @@
+import { sequelize } from '../../../../database';
+import { DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional } from 'sequelize';
+import { defaultImage } from '../constants/defaultImage';
+
+export interface ArtistInterface extends Model<InferAttributes<ArtistInterface>, InferCreationAttributes<ArtistInterface>> {
+  id: CreationOptional<string>;
+  name: string;
+  nationality: string;
+  image: string; 
+}
+
+export const Artist = sequelize.define<ArtistInterface>('Artist', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  nationality: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: defaultImage,
+  }
+});
+
+Artist.sync({alter: true, force: false})
+  .then(() => {
+    console.log('Artist table was (re)created');
+  })
+  .catch((err) => console.log(err));
+
