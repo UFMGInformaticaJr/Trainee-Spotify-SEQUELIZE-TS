d29cc97b5b82d89f8359bdfbd944cac48ad884fd
André Luiz Alves Costa <andrelac963@gmail.com>
2022-10-14 18:06:25 -0300

    fix: concertando a cara feia do arthur

---
+++ client/src/Navigation.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./MusicList.css";
+import SideBar from "./SideBar";
+import MusicList from "./MusicList";
+import ArtistsPage from "./ArtistsPage";
+import LoginPage from "./LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUpPage from "./SignUpPage";
+import UserAccountPage from "./UserAccountPage";
+
+export function Navigation() {
+    
+  return (
+    <div className="background">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/curtidas"
+            element={
+              <>
+                <MusicList />
+                <SideBar />
+              </>
+            }
+          />
+          <Route
+            path="/artists"
+            element={
+              <>
+                <ArtistsPage />
+                <SideBar />
+              </>
+            }
+          />
+          <Route
+            path="/useraccount"
+            element={
+              <>
+                <UserAccountPage />
+                <SideBar />
+              </>
+            }
+          />
+          <Route index element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
