f1b9d3108b716e4d6a40071f2ae62ed2445e07c8
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-10-18 18:24:02 -0300

    feat: extending Express and NodeJS

--- api/@types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { PayloadParams } from '../../src/domains/users/types/PayloadParams'; 
-
-declare global {
-    namespace Express {
-        interface Request{
-            user: PayloadParams
-        }
-    }
-}
