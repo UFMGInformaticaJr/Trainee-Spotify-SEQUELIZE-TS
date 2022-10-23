7214c38d7060394e64dbef416893732a8dc8a584
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-10-23 19:34:16 -0300

    feat: add Env interface

--- api/@types/global.d.ts
+++ api/@types/global.d.ts
@@ -1,13 +1,33 @@
 import { PayloadParams } from '../src/domains/users/types/PayloadParams'; 
-import { Env } from '../config/express-config';
+
+interface Env {
+    DB: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_HOST: string;
+    SECRET_KEY: string;
+    NODE_ENV: string;
+    JWT_EXPIRATION: string;
+    APP_URL: string;
+}
 
 declare global {
     namespace Express {
         interface Request{
-            user: PayloadParams
+            user?: PayloadParams
         }
     };
     namespace NodeJS {
         interface ProcessEnv extends Env {}
+        // interface ProcessEnv {
+        //     DB: string;
+        //     DB_USER: string;
+        //     DB_PASSWORD: string;
+        //     DB_HOST: string;
+        //     SECRET_KEY: string;
+        //     NODE_ENV: string;
+        //     JWT_EXPIRATION: string;
+        //     APP_URL: string;
+        // }
     };
 }
