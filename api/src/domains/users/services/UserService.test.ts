58e2327e02f639483555e4995ce02083f08b37ce
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-01-15 17:07:05 -0300

    feat: create test

--- api/src/domains/users/services/UserService.test.ts
+++ api/src/domains/users/services/UserService.test.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {User} from '../models/User';
 import {UserInterface} from '../models/User';
 import {UserService} from './UserService';
-
+import bcrypt from 'bcrypt';
 
 jest.mock('../models/User', () => ({
   User: {
@@ -13,7 +14,7 @@ jest.mock('../models/User', () => ({
   },
 }));
 
-
+const mockHash = jest.spyOn(bcrypt, 'hash');
 
 describe('getById', () => {
   beforeEach(() => {
@@ -46,4 +47,51 @@ describe('getById', () => {
     
   });
 
+});
+
+describe('create', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
+  test('método recebe um objeto com as informações do usuário => chama o create com os dados corretos', async () => {
+    const mockBodyUser = {
+      name: 'Teste',
+      email: '',
+      password: '123456',
+      role: 'user',
+    } as UserInterface;
+    
+    const mockCreateUser = {
+      name: 'Teste',
+      email: '',
+      password: '12345678',
+      role: 'user',
+    } as UserInterface;
+
+    mockHash.mockImplementation(() => '12345678');
+
+    (User.create as jest.MockedFunction<typeof User.create>).mockReturnValue({});
+    
+    await UserService.create(mockBodyUser);
+
+    expect(User.create).toBeCalledWith(mockCreateUser);
+    expect(User.create).toBeCalledTimes(1);
+  });
+
+  test('método recebe um usuário => cria um novo usuário com senha criptografada', async () => {
+    const mockBodyUser = {
+      name: 'Teste',
+      email: '',
+      password: '123456',
+      role: 'user',
+    } as UserInterface;
+
+    (User.create as jest.MockedFunction<typeof User.create>).mockReturnValue({});
+    const saltRounds = 10;
+    await UserService.create(mockBodyUser);
+
+    expect(mockHash.mock.calls[0]).toEqual([mockBodyUser.password, saltRounds]);
+  });
 });
