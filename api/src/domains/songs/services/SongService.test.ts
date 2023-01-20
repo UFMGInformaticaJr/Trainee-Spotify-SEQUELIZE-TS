ffec4600a606388daf28c694b3cbfbd586e127a1
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-01-20 19:23:02 -0300

    feat: getAll song

--- api/src/domains/songs/services/SongService.test.ts
+++ api/src/domains/songs/services/SongService.test.ts
@@ -36,3 +36,41 @@ describe('create', () => {
     expect(Song.create).toBeCalledTimes(1);
   });   
 });
+
+describe('getAll', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
+  test('método é chamado => retorna as músicas', async () => {
+    const mockSongs = [
+      {
+        title: 'Teste',
+        cover_image: 'Teste',
+        artist_id: 'Teste',
+        genre: 'Teste',
+      } as SongInterface,
+      {
+        title: 'Teste2',
+        cover_image: 'Teste2',
+        artist_id: 'Teste2',
+        genre: 'Teste2',
+      } as SongInterface,
+    ];
+    (Song.findAll as jest.MockedFunction<typeof Song.findAll>).mockResolvedValue(mockSongs);
+    
+    const songs = await SongService.getAll();
+
+    expect(Song.findAll).toBeCalledTimes(1);
+    expect(songs).toEqual(mockSongs);
+  });
+
+  test('método é chamado sem haver músicas no sistema => retorna um erro', async () => {
+    (Song.findAll as any).mockResolvedValue([]);
+    
+    await expect(SongService.getAll()).rejects.toThrow('Não há nenhuma música cadastrada');
+  });
+});
+
+
